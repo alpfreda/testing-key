6c411f7a906bccc976ff7230dcf06b9bbd38a9fb
Alpfreda <alpfreda@gmail.com>
2024-11-05 21:45:56 +0300

    tes2

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
 import './App.css';
 
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 const OtpForm = () => {
-  const inputRef = useRef(null);
-  const formRef = useRef(null);
+  // const inputRef = useRef(null);
+  // const formRef = useRef(null);
 
-  useEffect(() => {
-    const input = inputRef.current;
-    if (!input) return;
+  // useEffect(() => {
+  //   const input = inputRef.current;
+  //   if (!input) return;
 
-    // Check if the credentials API and OTP transport are supported
-    if (!navigator.credentials || !navigator.credentials.get) {
-      console.warn("The navigator.credentials API is not supported in this browser.");
-      return;
-    }
+  //   // Check if the credentials API and OTP transport are supported
+  //   if (!navigator.credentials || !navigator.credentials.get) {
+  //     console.warn("The navigator.credentials API is not supported in this browser.");
+  //     return;
+  //   }
 
-    const ac = new AbortController();
-    const form = formRef.current;
+  //   const ac = new AbortController();
+  //   const form = formRef.current;
 
-    const handleSubmit = () => {
-      ac.abort();
-    };
+  //   const handleSubmit = () => {
+  //     ac.abort();
+  //   };
 
-    if (form) {
-      form.addEventListener('submit', handleSubmit);
-    }
+  //   if (form) {
+  //     form.addEventListener('submit', handleSubmit);
+  //   }
 
-    navigator.credentials.get({
-      otp: { transport: ['sms'] },
-      signal: ac.signal
-    }).then(otp => {
-      input.value = otp.code;
-      if (form) form.submit();
-    }).catch(err => {
-      console.log(err);
-    });
+  //   navigator.credentials.get({
+  //     otp: { transport: ['sms'] },
+  //     signal: ac.signal
+  //   }).then(otp => {
+  //     input.value = otp.code;
+  //     if (form) form.submit();
+  //   }).catch(err => {
+  //     console.log(err);
+  //   });
 
-    return () => {
-      if (form) {
-        form.removeEventListener('submit', handleSubmit);
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (form) {
+  //       form.removeEventListener('submit', handleSubmit);
+  //     }
+  //   };
+  // }, []);
 
   return (
-    <form ref={formRef}>
-      <input ref={inputRef}  type="text" autoComplete="one-time-code" inputMode="numeric" name="one-time-code" />
-
-      <button type="submit">Submit</button>
-    </form>
+    // <form ref={formRef}>
+      <input type="text" autoComplete="one-time-code" inputMode="numeric" name="one-time-code" />
+      // <button type="submit">Submit</button>
+    // </form>
   );
 };
 
